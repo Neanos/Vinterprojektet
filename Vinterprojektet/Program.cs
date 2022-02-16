28dafc1a02e9b27283678ec51bd07fda5cc7acfb
Linnea Winqvist <winqvist.linnea@elev.ga.ntig.se>
2022-02-16 12:21:01 +0100

    första

---
+++ Vinterprojektet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "Snake");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.VIOLET);
+    Raylib.DrawRectangle(20, 20, 50, 50, Color.BEIGE);
+
+    Raylib.EndDrawing();
+}
